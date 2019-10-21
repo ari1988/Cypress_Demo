8b87b4837d1b2bd2ea7fe72911fd9df6dda31892
Arvind Singh <arvind.singh@web.com>
2019-10-21 18:36:58 +0530

    Updated Cypress_runner.js

--- cypress_runner.js
+++ cypress_runner.js
@@ -1,22 +1,9 @@
 const cypress = require('cypress');
-const cypressConfig = require('./cypress');
 const fse = require('fs-extra');
-//const merge = require('mochawesome-merge')
-//const marge = require('mochawesome-report-generator')
-//const reportDir = cypressConfig.reporterOptions.reportDir
-//const reportFiles = `${reportDir}/*.json`
 
 async function runTests() {
     await fse.emptyDir('report/mochawesome-report');
-    await cypress.run().then((results) =>{
-        const reporterOptions = {
-            reportDir: results.config.reporterOptions.reportDir,
-        }
-        
-    }).catch((error) => {
-        console.error('errors: ', error)
-        process.exit(1)
-    })
+    await cypress.run();
 
 }
 

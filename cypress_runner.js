9d730eecb51e425ddc85f0b51cc5152cfe7aa9db
Arvind Singh <arvind.singh@web.com>
2019-10-21 19:18:51 +0530

    Updated .gitignore file to ignore reports/results

--- cypress_runner.js
+++ cypress_runner.js
@@ -3,6 +3,7 @@ const fse = require('fs-extra');
 
 async function runTests() {
     await fse.emptyDir('report/mochawesome-report');
+    await fse.emptyDir('cypress/mochawesome-report');
     await cypress.run();
 
 }
